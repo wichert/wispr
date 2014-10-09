3f2a10da899c964f86b61ef764efc2fd1416f129
Wichert Akkerman <wichert@wiggy.net>
2014-10-09 11:29:32 +0200

    Packagingn fixes.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from setuptools import find_packages
 from setuptools import setup
 
 version = '1.0'
@@ -6,11 +7,23 @@ setup(
         name='wispr',
         description='WISPr command line client',
         long_description='',
-        classifiers=[],
+        keywords='wispr wifi hotspot',
+        classifiers=[
+            'Environment :: Console',
+            'License :: DFSG approved',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Intended Audience :: End Users/Desktop',
+        ],
         author='Wichert Akkerman',
         author_email='wichert@wiggy.net',
         url='https://github.com/wichert/wispr',
         license='BSD',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        install_requires=[
+            'requests',
+        ],
         entry_points='''
         [console_scripts]
         wispr = wispr:main
